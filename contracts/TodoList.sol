a7bde347e91c5210b3275b97839d2094f49d91b5
Natasha Camille <nnalubega@outlook.com>
2024-04-27 00:23:48 +0300

    Creating a list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,26 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+/// Create the TodoList 
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    } 
+
+/// Storage
+
+mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com ");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
